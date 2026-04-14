6ce2c2d07ea140ef19e330169554a41fcd0610e0
Nik Kho <nikkhowork@gmail.com>
2026-04-14 18:26:24 +0000

    fix over fix

--- tf.sh
+++ tf.sh
@@ -4,7 +4,8 @@ docker run --rm \
   --user $(id -u):$(id -g) \
   --network host \
   -v $(pwd):/workspace \
-  -v $HOME/.kube:/root/.kube \
+  -v $HOME/.kube:/tmp/.kube \
   -v /var/run/docker.sock:/var/run/docker.sock \
+  -e KUBECONFIG=/tmp/.kube/config \
   -w /workspace/infra \
   hashicorp/terraform:latest "$@"
